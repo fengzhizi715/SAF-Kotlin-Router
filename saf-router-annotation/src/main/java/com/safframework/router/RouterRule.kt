723fc714db8c3175e9be5ffe17d184dbd19d9804
Tony Shen <fengzhizi715@126.com>
2017-01-19 00:18:31 +0800

    修复RouterRule.kt的bug

--- saf-router-annotation/src/main/java/com/safframework/router/RouterRule.kt
+++ saf-router-annotation/src/main/java/com/safframework/router/RouterRule.kt
@@ -1,14 +1,11 @@
 package com.safframework.router
 
-import java.lang.annotation.ElementType
 import java.lang.annotation.Retention
 import java.lang.annotation.RetentionPolicy
-import java.lang.annotation.Target
 
 /**
  * Created by Tony Shen on 2017/1/18.
  */
-@Target(ElementType.TYPE)
 @Retention(RetentionPolicy.CLASS)
 annotation class RouterRule(
         /** activity对应url  */

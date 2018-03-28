1d5cd83c5eb94323dd5912efc8034cf8090cb769
fengzhizi715 <fengzhizi715@126.com>
2018-03-28 17:29:48 +0800

    update MatchType

--- saf-router/src/main/java/com/safframework/router/Mapping.java
+++ saf-router/src/main/java/com/safframework/router/Mapping.java
@@ -1,7 +1,6 @@
 package com.safframework.router;
 
 import android.app.Activity;
-import android.net.Uri;
 
 /**
  * @version V1.0 <描述当前版本功能>
@@ -15,6 +14,7 @@ public class Mapping {
     private Class<? extends Activity> activity;
     private RouterParameter.RouterOptions options;
     private MethodInvoker method;
+    private MatchType matchType;
 
     public Mapping(String format,Class<? extends Activity> activity,RouterParameter.RouterOptions options,MethodInvoker method) {
 
@@ -26,6 +26,13 @@ public class Mapping {
         this.options = options;
         this.method = method;
 
+        if (format.toLowerCase().startsWith("http://") || format.toLowerCase().startsWith("https://")) {
+            matchType = MatchType.BROWSER;
+        } else if (format.contains("://")) {
+            matchType = MatchType.SCHEME;
+        } else {
+            matchType = MatchType.PATH_ACTITY;
+        }
     }
 
     public String getFormat() {
---
+++ saf-router/src/main/java/com/safframework/router/MatchType.java
@@ -0,0 +1,16 @@
+package com.safframework.router;
+
+/**
+ * @version V1.0 <描述当前版本功能>
+ * @FileName: com.safframework.router.MatchType.java
+ * @author: Tony Shen
+ * @date: 2018-03-28 17:10
+ */
+public enum MatchType {
+
+    BROWSER,
+    SCHEME,
+    PATH_ACTITY,
+    PATH_FRAGMNET,
+    PATH_ACTION
+}
